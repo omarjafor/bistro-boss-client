a61688dee67aace3eb92d50961613de2f0f26561
omarjafor <omarjaforchy@gmail.com>
2023-11-17 21:46:57 +0600

    some components added with banner

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
     </>
     
     return (
-        <div className="navbar bg-orange-600 text-white">
+        <div className="navbar sticky z-10 top-0 bg-orange-600 text-white">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
