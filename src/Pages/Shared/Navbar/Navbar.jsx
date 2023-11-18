47c898dc9ed63f4a4828d3e3a3b112c389ff6af1
omarjafor <omarjaforchy@gmail.com>
2023-11-18 10:22:44 +0600

    featured style and components navbar

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
     </>
     
     return (
-        <div className="navbar sticky z-10 top-0 bg-orange-600 text-white">
+        <div className="navbar sticky z-10 top-0 bg-orange-600 lg:text-white text-black">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
