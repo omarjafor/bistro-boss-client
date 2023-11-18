310262e3d7edc84d1e76ada1179179886315bba8
omarjafor <omarjaforchy@gmail.com>
2023-11-18 09:59:03 +0600

    featured style and components

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -1,11 +1,11 @@
 
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
 import featuredImg from '../../../assets/home/featured.jpg';
-
+import './Featured.css';
 
 const Featured = () => {
     return (
-        <div className="bg-[url('../../../assets/home/featured.jpg')] bg-fixed text-white pt-8 my-20">
+        <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle subHeading="check it out" heading="Featured Item" ></SectionTitle>
             <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-60 pb-20 pt-12 px-36">
                 <div>
