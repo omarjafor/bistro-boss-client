148b52d3ebda995ef450181f1a7ed8f1305b56d1
omarjafor <omarjaforchy@gmail.com>
2023-11-18 15:28:49 +0600

    react components with menu and cover

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../LayOut/Main";
 import Home from "../Pages/Home/Home";
+import Menu from "../Pages/Menu/Menu";
 
 const Router = createBrowserRouter([
     {
@@ -10,6 +11,10 @@ const Router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: 'menu',
+                element: <Menu></Menu>
             }
         ]
     },
