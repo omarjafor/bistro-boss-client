71e4834a6fc25faf10b4da570f7158c5e7703ea9
omarjafor <omarjaforchy@gmail.com>
2023-11-19 18:49:58 +0600

    server side implemented now data from database

--- src/Pages/Order/OrderTab/OrderTab.jsx
+++ src/Pages/Order/OrderTab/OrderTab.jsx
@@ -22,15 +22,34 @@ const OrderTab = ({ items }) => {
                 className="mySwiper"
             >
                 <SwiperSlide>
-                    <div className='grid md:grid-cols-3 gap-10'>
+                    <div className='grid md:grid-cols-3 gap-10 my-10'>
                         {
-                            items.map(item => <FoodCard
+                            items.slice(0, 6).map(item => <FoodCard
                                 key={item._id}
                                 item={item}
                             ></FoodCard>)
                         }
                     </div>
-
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='grid md:grid-cols-3 gap-10 my-10'>
+                        {
+                            items.slice(6, 12).map(item => <FoodCard
+                                key={item._id}
+                                item={item}
+                            ></FoodCard>)
+                        }
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className='grid md:grid-cols-3 gap-10 my-10'>
+                        {
+                            items.slice(12, 18).map(item => <FoodCard
+                                key={item._id}
+                                item={item}
+                            ></FoodCard>)
+                        }
+                    </div>
                 </SwiperSlide>
             </Swiper>
         </div>
