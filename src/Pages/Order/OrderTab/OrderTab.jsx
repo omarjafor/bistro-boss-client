4f61792320628b5d95b3a3aa9d816efb9b937ed0
omarjafor <omarjaforchy@gmail.com>
2023-11-19 10:11:27 +0600

    react commponents food card added

--- src/Pages/Order/OrderTab/OrderTab.jsx
+++ src/Pages/Order/OrderTab/OrderTab.jsx
@@ -1,9 +1,38 @@
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from 'swiper/modules';
+import "swiper/css";
+import "swiper/css/pagination";
+import FoodCard from "../../../Components/FoodCard/FoodCard";
 
-const OrderTab = () => {
+const OrderTab = ({ items }) => {
+
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    };
+
     return (
-        <div>
-            
+        <div >
+            <Swiper
+                pagination={pagination}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className='grid md:grid-cols-3 gap-10'>
+                        {
+                            items.map(item => <FoodCard
+                                key={item._id}
+                                item={item}
+                            ></FoodCard>)
+                        }
+                    </div>
+
+                </SwiperSlide>
+            </Swiper>
         </div>
     );
 };
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     const navOptions = <>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/menu">Our Menu</Link></li>
-        <li><Link to="/order">Order Food</Link></li>
+        <li><Link to="/order/salad">Order Food</Link></li>
         <li><Link to="/secret">Secret</Link></li>
         <li>
             <Link to="/dashboard/cart">
