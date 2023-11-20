22812f11d839408117f7cac66c9e5c8078a1564e
omarjafor <omarjaforchy@gmail.com>
2023-11-20 17:13:24 +0600

    sign up register with react hook form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -90,7 +90,7 @@ const Login = () => {
                             </div>
                         </form>
                         <p className='px-6'><small>New Here? <Link to="/signup">Create an account</Link> </small></p>
-                        <SocialLogin></SocialLogin>
+                        {/* <SocialLogin></SocialLogin> */}
                     </div>
                 </div>
             </div>
