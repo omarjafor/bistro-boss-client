3214b4a07f31891644901a54ec485d39dfd19243
omarjafor <omarjaforchy@gmail.com>
2023-11-20 23:18:29 +0600

    private route components for secret info

---
+++ src/Pages/Shared/Secret/Secret.jsx
@@ -0,0 +1,11 @@
+
+
+const Secret = () => {
+    return (
+        <div>
+            <h2 className="text-center text-4xl font-bold">This is a Secret Area</h2>
+        </div>
+    );
+};
+
+export default Secret;
