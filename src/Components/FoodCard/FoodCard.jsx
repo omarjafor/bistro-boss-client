b9bc1f9045b596e0c0e2221ee7796b605cabba54
omarjafor <omarjaforchy@gmail.com>
2023-11-20 23:19:19 +0600

    private route components for secret info

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,8 +1,40 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../../Hooks/useAuth";
+import Swal from "sweetalert2";
 
 
 const FoodCard = ({ item }) => {
-    const { name, image, price, recipe } = item;
+    const { name, image, price, recipe, _id } = item;
+    const { user } = useAuth();
+    const navigate = useNavigate();
+    const location = useLocation();
 
+    const handleAddToCart = food => {
+        if( user && user?.email){
+            console.log(user.email, food);
+            const cartItem = {
+                menuId: _id,
+                email: user?.email,
+                name,
+                image,
+                price
+            }
+        }else{
+            Swal.fire({
+                title: "You are not Logged In",
+                text: "Please login for add to the cart?",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, login!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login', { state: {from: location}})
+                }
+            });
+        }
+    }
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
@@ -12,7 +44,7 @@ const FoodCard = ({ item }) => {
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
                     <button
-                        
+                        onClick={() => handleAddToCart(item)}
                         className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4"
                     >Add to Cart</button>
                 </div>
