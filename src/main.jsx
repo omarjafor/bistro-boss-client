c692af2c7506fc9631963c74584ead9c7c8b5144
omarjafor <omarjaforchy@gmail.com>
2023-11-21 20:00:34 +0600

    admin dashboard cart item get etc

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,18 @@ import { RouterProvider } from "react-router-dom";
 import Router from './Routes/Router';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProviders from './Providers/AuthProviders';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProviders>
-      <HelmetProvider>
-        <RouterProvider router={Router} />
-      </HelmetProvider>
-    </AuthProviders>
-  </React.StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <AuthProviders>
+        <HelmetProvider>
+          <RouterProvider router={Router} />
+        </HelmetProvider>
+      </AuthProviders>
+    </QueryClientProvider>
+  </React.StrictMode >,
 )
