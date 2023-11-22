be55824580132cbfeab5ffe0b2f5e7325dda9b61
omarjafor <omarjaforchy@gmail.com>
2023-11-22 20:23:06 +0600

    admin route update manage user items etc

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import auth from "../Firebase/firebase.config";
+import useAxiosPublic from "../Hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 
@@ -8,6 +9,7 @@ const AuthProviders = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password) => {
         setLoading(true);
@@ -38,12 +40,23 @@ const AuthProviders = ({ children }) => {
     useEffect( () => {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+            if(currentUser){
+                const userInfo = { email: currentUser.email };
+                axiosPublic.post('/jwt', userInfo)
+                .then(res => {
+                    if(res.data.token){
+                        localStorage.setItem('access-token', res.data.token)
+                    }
+                })
+            }else{
+                localStorage.removeItem('access-token');
+            }
             setLoading(false);
         })
         return () => {
             unSubscribe();
         }
-    } , [])
+    } , [axiosPublic])
 
     const authInfo = {
         user,
